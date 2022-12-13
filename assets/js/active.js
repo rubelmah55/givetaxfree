4e5c23dec887e43705abe21bdc83835d493ee7e3
RajuAhmed2 <92273686+RajuAhmed2@users.noreply.github.com>
2022-12-14 03:12:33 +0600

    first commit

--- assets/js/active.js
+++ assets/js/active.js
@@ -1,124 +1,15 @@
-/*
-Template: BUSICO - Multipurpose Bootstrap 5 Template
-Author: RRDevs
-*/
 
 (function($) {
     "use strict";
 
     $(document).ready( function() {
 
-        /* =============================================
-            # Magnific popup init
-        ===============================================*/
-
-        const $owlCarousel = $(".agency-slider-active").owlCarousel({        
-            items: 1,     
-            dots: true,
-            loop: true,
-            autoplayTimeout: 9000,
-            autoplay:true,
-        });
-
-        $(".owl-carousel").on("initialized.owl.carousel", () => {
-            setTimeout(() => {
-                $(".owl-item.active .animated-text").addClass("is-transitioned");
-            }, 200);
-        });
-
-        $owlCarousel.on("changed.owl.carousel", e => {
-            $(".animated-text").removeClass("is-transitioned");
-            
-            const $currentOwlItem = $(".owl-item").eq(e.item.index);
-            $currentOwlItem.find(".animated-text").addClass("is-transitioned");
-            
-            const $target = $currentOwlItem.find(".hero-contents");
-        });
-
-        $(".testimonial-carousel-list").owlCarousel({        
-            items: 1,     
-            dots: false,
-            loop: true,
-            autoplayTimeout: 8000,
-            autoplay:true,
-            nav: true,     
-            navText: ['<i class="fal fa-long-arrow-left"></i>', '<i class="fal fa-long-arrow-right"></i>'],
-        });
-        
-        $(".brand-carousel-active").owlCarousel({ 
-            margin: 80,
-            loop: true,
-            autoplayTimeout: 5000,
-            autoplay:true,                                                                          
-            responsive : {
-                // breakpoint from 0 up
-                0 : {
-                    items: 2,                    
-                },
-                // breakpoint from 768 up
-                480 : {
-                    items: 3
-                },
-                // breakpoint from 768 up
-                768 : {
-                    items: 3
-                },
-                // breakpoint from 992 up
-                992 : {
-                    items: 4
-                },
-                
-                1191 : {
-                    items: 5
-                },
-            }
-        });
-
-        /* =============================================
-            # Magnific popup init
-         ===============================================*/
-        
-        $(".popup-link").magnificPopup({
-            type: 'image',
-            fixedContentPos: false
-        });
-
-        $(".popup-gallery").magnificPopup({
-            type: 'image',
-            fixedContentPos: false,
-            gallery: {
-                enabled: true
-            },
-            // other options
-        });
-
-        $(".popup-video, .popup-vimeo, .popup-gmaps").magnificPopup({
-            type: "iframe",
-            mainClass: "mfp-fade",
-            removalDelay: 160,
-            preloader: false,
-            fixedContentPos: false
-        });
-        
-        /*==========================
-           Scroll To Up Init
-        ============================*/
-        // $.scrollUp({
-        //     scrollName: 'scrollUp', // Element ID
-        //     topDistance: '1110', // Distance from top before showing element (px)
-        //     topSpeed: 2000, // Speed back to top (ms)
-        //     animation: 'slide', // Fade, slide, none
-        //     animationInSpeed: 300, // Animation in speed (ms)
-        //     animationOutSpeed: 300, // Animation out speed (ms)
-        //     scrollText: '<i class="fas fa-angle-up"></i>', // Text for element
-        //     activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-        // });
 
         // Sticky Menu
         $(window).scroll(function() {
             var Width = $(document).width();
 
-            if ($("body").scrollTop() > 100 || $("html").scrollTop() > 100) {
+            if ($("body").scrollTop() > 10 || $("html").scrollTop() > 10) {
                 if (Width > 767) {
                     $("header").addClass("sticky");
                 }
@@ -127,29 +18,7 @@ Author: RRDevs
             }
         });
 
-        $('.container').imagesLoaded(function() {
-            $('.portfolio-cat-filter').on('click', 'button', function() {
-                var filterValue = $(this).attr('data-filter');
-                $grid.isotope({ filter: filterValue });
-            });
-
-            var $grid = $('.grid').isotope({
-                itemSelector: '.grid-item',
-                percentPosition: true,
-                masonry: {
-                    columnWidth: '.grid-item'
-                }
-            });
-        });
-
-        var catButton = '.portfolio-cat-filter button';
-
-        $(catButton).on('click', function(){
-            $(catButton).removeClass('active');
-            $(this).addClass('active');
-        });
-
-
+         /*** Mobile Menu */
         $('#hamburger').on('click', function() {            
             $('.mobile-nav').addClass('show');
             $('.overlay').addClass('active');
@@ -167,39 +36,6 @@ Author: RRDevs
 
         $("#mobile-menu").metisMenu();
 
-        new WOW().init();
-
-        const counterUp = window.counterUp.default
-
-        const callback = entries => {
-            entries.forEach( entry => {
-                const el = entry.target
-                if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
-                    counterUp( el, {
-                        duration: 3500,
-                        delay: 15,
-                    } )
-                    el.classList.add( 'is-visible' )
-                }
-            } )
-        }
-
-        const IO = new IntersectionObserver( callback, { threshold: 1 } )
-
-        const el = document.querySelectorAll( '.single-fun-fact span' );
-        el.forEach((el) => {
-            IO.observe(el);
-        });
-
-
-        $('.side-menu-toggle, .offcanvas-btn').on('click', function() {            
-            $('.offset-menu').addClass('show');
-        });
-
-        $('#offset-menu-close-btn').on('click', function() {            
-            $('.offset-menu').removeClass('show');
-        });
-
         /*** Search bar */
         $('.header-search').on('click', '.search-toggle', function(e) {
             e.preventDefault();
@@ -208,15 +44,6 @@ Author: RRDevs
         });
         
     }); // end document ready function
-
-    function loader() {
-        $(window).on('load', function() {
-            // Animate loader off screen
-            $(".preloader").addClass('loaded');                    
-            $(".preloader").delay(600).fadeOut();                       
-        });
-    }
-    loader();
 
 
 })(jQuery); // End jQuery
