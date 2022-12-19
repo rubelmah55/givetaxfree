8ca8d8ef2d0022d9600c491144d2aade4c63e910
RajuAhmed2 <92273686+RajuAhmed2@users.noreply.github.com>
2022-12-19 16:47:52 +0600

    add file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -52,42 +52,6 @@
                   slidesToShow: 2,
                   slidesToScroll: 2,
                   infinite: true,
-                  dots: true
-                }
-              },
-              {
-                breakpoint: 600,
-                settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 2
-                }
-              },
-              {
-                breakpoint: 480,
-                settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1
-                }
-              }
-            ]
-          });
-
-
-        $('.single_slider').slick({
-            dots: true,
-            infinite: true,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            speed: 300,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            responsive: [
-              {
-                breakpoint: 1024,
-                settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 1,
-                  infinite: true,
                   dots: false
                 }
               },
