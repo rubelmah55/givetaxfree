dacf78ec8a2bd56c942540d3b12b1ffaedc547ef
RajuAhmed2 <92273686+RajuAhmed2@users.noreply.github.com>
2023-01-12 17:42:18 +0600

    first commit

--- home.php
+++ home.php
@@ -292,32 +292,83 @@ Template Name: Home
             </div>
         </div>
         <div class="row">
+
             <div class="col-lg-4 col-md-6">
                 <div class="single_feaured_item" data-aos="fade-up">
-                    <div class="popular-causes__img bg-center bg-cover" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/img/pople_03.png);"></div>
+                    <div class="popular-causes__img bg-center bg-cover" style="background-image: url( <?php echo get_stylesheet_directory_uri(); ?>/assets/img/pople_03.png);"></div>
                     <div class="popular-causes__content">
                         <div class="popular-causes__title">
-                            <span>#Donation</span>
                             <h3><a href="campaign-details.html">Donner Poor Children</a></h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed facere explicabo et reiciendis </p>
                         </div>
-                        <div class="popular-causes__progress">
-                            <div class="popular-causes__goals d-flex justify-content-between align-items-center">
-                                <p>Raised: <span>$25,270</span></p>
-                                <p>Goal: <span>$30,000</span></p>
+                        <div class="popular_bar">
+                            <div class="popular-causes__progress">
+                                <div class="popular-causes__goals d-flex justify-content-between align-items-center">
+                                    <p>Raised: <span>$25,270</span></p>
+                                    <p>Goal: <span>$30,000</span></p>
+                                </div>
+    
+                                <div class="bar">
+                                    <div class="bar-inner count-bar counted" data-percent="36%" style="width: 36%;"></div>
+                                </div>
+                                
                             </div>
-
-                            <div class="bar">
-                                <div class="bar-inner count-bar counted" data-percent="36%" style="width: 36%;"></div>
-                            </div>
-                            
+                            <a href="#" class="theme-btn">Donate Now <i class="fas fa-angle-right"></i></a>
                         </div>
-                        <p>Reside by 40 papule 12 papule</p>
-                        <a href="#" class="theme-btn">Donate Now <i class="fas fa-angle-right"></i></a>
                     </div>
                 </div>
             </div>
+
             <div class="col-lg-4 col-md-6">
+                <div class="single_feaured_item" data-aos="fade-up">
+                    <div class="popular-causes__img bg-center bg-cover" style="background-image: url( <?php echo get_stylesheet_directory_uri(); ?>/assets/img/pople_03.png);"></div>
+                    <div class="popular-causes__content">
+                        <div class="popular-causes__title">
+                            <h3><a href="campaign-details.html">Donner Poor Children</a></h3>
+                        </div>
+                        <div class="popular_bar">
+                            <div class="popular-causes__progress">
+                                <div class="popular-causes__goals d-flex justify-content-between align-items-center">
+                                    <p>Raised: <span>$25,270</span></p>
+                                    <p>Goal: <span>$30,000</span></p>
+                                </div>
+    
+                                <div class="bar">
+                                    <div class="bar-inner count-bar counted" data-percent="36%" style="width: 36%;"></div>
+                                </div>
+                                
+                            </div>
+                            <a href="#" class="theme-btn">Donate Now <i class="fas fa-angle-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+                <div class="single_feaured_item" data-aos="fade-up">
+                    <div class="popular-causes__img bg-center bg-cover" style="background-image: url( <?php echo get_stylesheet_directory_uri(); ?>/assets/img/pople_03.png);"></div>
+                    <div class="popular-causes__content">
+                        <div class="popular-causes__title">
+                            <h3><a href="campaign-details.html">Donner Poor Children</a></h3>
+                        </div>
+                        <div class="popular_bar">
+                            <div class="popular-causes__progress">
+                                <div class="popular-causes__goals d-flex justify-content-between align-items-center">
+                                    <p>Raised: <span>$25,270</span></p>
+                                    <p>Goal: <span>$30,000</span></p>
+                                </div>
+    
+                                <div class="bar">
+                                    <div class="bar-inner count-bar counted" data-percent="36%" style="width: 36%;"></div>
+                                </div>
+                                
+                            </div>
+                            <a href="#" class="theme-btn">Donate Now <i class="fas fa-angle-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- <div class="col-lg-4 col-md-6">
                 <div class="single_feaured_item" data-aos="fade-up" data-aos-delay="100">
                     <div class="popular-causes__img bg-center bg-cover" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/img/pople_03.png);"></div>
                     <div class="popular-causes__content">
@@ -366,7 +417,7 @@ Template Name: Home
                         <a href="#" class="theme-btn">Donate Now <i class="fas fa-angle-right"></i></a>
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </section>
