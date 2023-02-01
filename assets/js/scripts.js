34674cb42f7f67f8aab10bef3058eab71708143b
RajuAhmed2 <92273686+RajuAhmed2@users.noreply.github.com>
2023-02-02 00:45:58 +0600

    first commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,18 @@
 (function($) {
     "use strict";
 
-    $(document).ready( function() {
-        // Sticky Menu
+    $(document).ready(function () {
+        
+        /* =============================================
+            # sticky Menu
+        ===============================================*/
         $(window).scroll(function() {
             var Width = $(document).width();
 
             if ($("body").scrollTop() > 10 || $("html").scrollTop() > 10) {
-                if (Width > 767) {
+                if (Width > 200) {
                     $("header").addClass("sticky");
-                } else if(Width < 767) {
+                } else if(Width < 200) {
                     $("header").addClass("hide");
                 }
             } else {
@@ -17,15 +20,18 @@
             }
         });
 
-         /*** Mobile Menu */
-        $('#hamburger').on('click', function() {            
+        /* =============================================
+            # Mobile Menu
+        ===============================================*/
+
+        $('#hamburger').on('click', function() {
             $('.mobile-nav').addClass('show');
             $('.overlay').addClass('active');
         });
 
-        $('.close-nav').on('click', function() {            
-            $('.mobile-nav').removeClass('show');            
-            $('.overlay').removeClass('active');          
+        $('.close-nav').on('click', function() {
+            $('.mobile-nav').removeClass('show');
+            $('.overlay').removeClass('active');
         });
 
         $(".overlay").on("click", function () {
@@ -35,39 +41,57 @@
 
         $("#mobile-menu").metisMenu();
 
-        AOS.init({
-            disable: function () {
-            var maxWidth = 768;
-            return window.innerWidth < maxWidth;
-        }});
-
-
-        $('.image-popup-vertical-fit').magnificPopup({
-            type: 'image',
-          mainClass: 'mfp-with-zoom', 
-          gallery:{
-                    enabled:true
-                },
-          zoom: {
-            enabled: true, 
-        
-            duration: 300, 
-            easing: 'ease-in-out',
-        
-            opener: function(openerElement) {
-        
-              return openerElement.is('img') ? openerElement : openerElement.find('img');
-          }
-        }
-        
+         /* =============================================
+            # Filter Init
+        ===============================================*/
+        $('.tab-btn').click(function() {
+            // Remove active class from currently active button and content
+            $('.tab-btn').removeClass('active');
+            $('.tab-content div').removeClass('active');
+            
+            // Add active class to clicked button and corresponding content
+            $(this).addClass('active');
+            $('.' + $(this).attr('id')).addClass('active');
         });
 
-        var slider = document.getElementById("myRange");
+        /* =============================================
+            # AOS Init
+        ===============================================*/
+        AOS.init();
+
+        /* =============================================
+            # AOS Init
+        ===============================================*/
+        $('.tab-btn').click(function() {
+          $('.tab-btn').removeClass('active');
+          $(this).addClass('active');
+          
+          $('.tab-content').removeClass('active');
+          $('#' + $(this).data('tab')).addClass('active');
+        });
+        
+        /* =============================================
+            # Magnific popup
+        ===============================================*/
+        $('.image-popup-vertical-fit').magnificPopup({
+            type: 'image',
+            mainClass: 'mfp-with-zoom',
+            gallery:{
+                    enabled:true,
+                },
+            zoom: {
+                enabled: true,
+                duration: 300,
+                easing: 'ease-in-out',
+                opener: function(openerElement) {
+                    return openerElement.is('img') ? openerElement : openerElement.find('img');
+                }
+            }
+        });
+
+        //Rang Bar
         var output = document.getElementById("value_rang");
         var value_tup = document.getElementById("value_tup");
-        // output.innerHTML = slider.value; 
-
-       
 
         $("#myRange").on("input", function(){
             if (this.value == 5){
@@ -83,40 +107,38 @@
             }
             value_tup.innerHTML = this.value+"%";
         })
-    
-        
-        // tabb
 
         $(".login_btn").click(function(){
             $(".drop").toggle();
         });
-    }); // end document ready function
 
-    // search popup
-    $(".search_bar").on('click', function(){
-        $(".overlay-hugeinc").addClass("open");
-    });
+         //search popup
+        $(".search_bar").on('click', function(){
+            $(".overlay-hugeinc").addClass("open");
+        });
 
-    $(".overlay-close").on('click', function(){
-        $(".overlay-hugeinc").removeClass("open");
-    });
+        $(".overlay-close").on('click', function(){
+            $(".overlay-hugeinc").removeClass("open");
+        });
 
-    // see all
-    $(".share_2").on('click', function(){
-        $(".overly_popup_2").addClass("show");
-    });
+        // see all
+        $(".share_2").on('click', function(){
+            $(".overly_popup_2").addClass("show");
+        });
 
-    $(".head_popup_2").on('click', function(){
-        $(".overly_popup_2").removeClass("show");
-    });
+        $(".head_popup_2").on('click', function(){
+            $(".overly_popup_2").removeClass("show");
+        });
 
-    // see top donation
-    $(".share_3").on('click', function(){
-        $(".overly_popup_3").addClass("show");
-    });
+        // see top donation
+        $(".share_3").on('click', function(){
+            $(".overly_popup_3").addClass("show");
+        });
 
-    $(".head_popup_3").on('click', function(){
-        $(".overly_popup_3").removeClass("show");
+        $(".head_popup_3").on('click', function(){
+            $(".overly_popup_3").removeClass("show");
+        });
+        
     });
 
     $(document).on(
@@ -126,4 +148,4 @@
         }
     });
 
-})(jQuery); // End jQuery
+})(jQuery);//Jquery end
--- home.php
+++ home.php
@@ -367,57 +367,6 @@ Template Name: Home
                     </div>
                 </div>
             </div>
-
-            <!-- <div class="col-lg-4 col-md-6">
-                <div class="single_feaured_item" data-aos="fade-up" data-aos-delay="100">
-                    <div class="popular-causes__img bg-center bg-cover" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/img/pople_03.png);"></div>
-                    <div class="popular-causes__content">
-                        <div class="popular-causes__title">
-                            <span>#Donation</span>
-                            <h3><a href="campaign-details.html">Donner Poor Children</a></h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed facere explicabo et reiciendis </p>
-                        </div>
-                        <div class="popular-causes__progress">
-                            <div class="popular-causes__goals d-flex justify-content-between align-items-center">
-                                <p>Raised: <span>$25,270</span></p>
-                                <p>Goal: <span>$30,000</span></p>
-                            </div>
-
-                            <div class="bar">
-                                <div class="bar-inner count-bar counted" data-percent="36%" style="width: 36%;"></div>
-                            </div>
-                            
-                        </div>
-                        <p>Reside by 40 papule 12 papule</p>
-                        <a href="#" class="theme-btn">Donate Now <i class="fas fa-angle-right"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="single_feaured_item" data-aos="fade-up" data-aos-delay="150">
-                    <div class="popular-causes__img bg-center bg-cover" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/img/pople_03.png);"></div>
-                    <div class="popular-causes__content">
-                        <div class="popular-causes__title">
-                            <span>#Donation</span>
-                            <h3><a href="campaign-details.html">Donner Poor Children</a></h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed facere explicabo et reiciendis </p>
-                        </div>
-                        <div class="popular-causes__progress">
-                            <div class="popular-causes__goals d-flex justify-content-between align-items-center">
-                                <p>Raised: <span>$25,270</span></p>
-                                <p>Goal: <span>$30,000</span></p>
-                            </div>
-
-                            <div class="bar">
-                                <div class="bar-inner count-bar counted" data-percent="36%" style="width: 36%;"></div>
-                            </div>
-                            
-                        </div>
-                        <p>Reside by 40 papule 12 papule</p>
-                        <a href="#" class="theme-btn">Donate Now <i class="fas fa-angle-right"></i></a>
-                    </div>
-                </div>
-            </div> -->
         </div>
     </div>
 </section>
